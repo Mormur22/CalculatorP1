1c4ad2ba4951dec6fe8a22dd5f01793e4ad0bebd
manuespi <manuespi@ucm.es>
2023-02-23 13:40:24 +0100

    Jueves 23/02/23

--- app/src/main/java/es/ucm/fdi/calculator/MainActivity.java
+++ app/src/main/java/es/ucm/fdi/calculator/MainActivity.java
@@ -4,9 +4,11 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         calculator = new Calculator();
+        Log.d("MainActivity", "Se ha creado bien la calculadora");
         editTextx = findViewById(R.id.numero1);
         editTexty = findViewById(R.id.numero2);
         button = findViewById(R.id.button);
@@ -27,14 +30,21 @@ public class MainActivity extends AppCompatActivity {
 
     public void addXandY(){
         Intent intent = new Intent(this, CalculatorResultActivity.class);
-        double res=calculator.suma(Double.parseDouble(this.editTextx.getText().toString()),Double.parseDouble(this.editTexty.getText().toString()));
-        intent.putExtra("suma",res);
-        startActivity(intent);
+        double res = -1;
+        try {
+            res=calculator.suma(Double.parseDouble(this.editTextx.getText().toString()),Double.parseDouble(this.editTexty.getText().toString()));
+            intent.putExtra("suma",res);
+            startActivity(intent);
+        }
+        catch(Exception nfe){
+            Log.e("MainActivity","Mete todos los valores: " + nfe.toString());
+        }
     }
 
     public void buttonListener(){
         button.setOnClickListener(new View.OnClickListener() {
             @Override
+
             public void onClick(View view) {
                 addXandY();
             }
